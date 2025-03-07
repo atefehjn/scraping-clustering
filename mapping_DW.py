4c10fceacccda4cfe1a61b322fcdc83aed3139ea
Atefe Hajian <atefe_hajian97@yahoo.com>
2025-03-07 23:48:59 +0330

    DW add

---
+++ mapping_DW.py
@@ -0,0 +1,19 @@
+digikala_mapping = {
+    "model_code": [ "model_code"],
+    "price": ["price"],
+    "ارتفاع": ["Height"],
+    "عمق": ["Depth"],
+    "پهنا":[ "Width"],
+    "امکانات ویژه":[ "Special Features"],
+    "سیستم ایمنی": ["Safety System"],
+    "تعداد سبد": "Number of Baskets",
+    "ظرفیت (نفر)": "Capacity (People)",
+    "وزن": "Weight",
+    "میزان صدا": "Noise Level",
+    "نوع ماشین ظرفشویی": "Dishwasher Type",
+    "متوسط میزان مصرف آب در هر شست و شو": "Average Water Consumption per Wash",
+    "تعداد قفسه های بالا": "Number of Upper Racks",
+    "تعداد برنامه های شست‌وشو": "Number of Washing Programs",
+    "نمودار میزان مصرف انرژی": "Energy Consumption Chart",
+    "جایگاه نگهداری": "Storage Position"
+}
